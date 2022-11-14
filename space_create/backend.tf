31c3d62fd45672f25f5633c7efa2cb9145409267
Matthew Casperson <matthewcasperson@gmail.com>
2022-11-14 13:56:31 +1000

    Space create and init now work

--- space_create/backend.tf
+++ space_create/backend.tf
@@ -1,6 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "#{Terraform.StateBucket}"
+    # This value must be unique for each space created. A value like:
+    # new-space-#{Octopus.Space.Name | ToLower | Replace " " "-"}
+    # works well.
     key    = "#{Terraform.NewSpace.StateBucketKey}"
   }
 }
--- space_init/backend.tf
+++ space_init/backend.tf
@@ -1,6 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "#{Terraform.StateBucket}"
+    # This value must be unique for each space created. A value like:
+    # space-init-#{Octopus.Space.Name | ToLower | Replace " " "-"}
+    # works well.
     key    = "#{Terraform.SpaceInit.StateBucketKey}"
   }
 }
