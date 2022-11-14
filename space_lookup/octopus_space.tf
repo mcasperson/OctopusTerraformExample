cda3660a178c4012d46e9d7f63fb2b4eb60f6ba8
Matthew Casperson <matthewcasperson@gmail.com>
2022-11-14 14:34:09 +1000

    Sigh

--- space_lookup/octopus_space.tf
+++ space_lookup/octopus_space.tf
@@ -1,5 +1,7 @@
+# https://registry.terraform.io/providers/OctopusDeployLabs/octopusdeploy/latest/docs/data-sources/spaces
+# Look up the space from the space name. This is used to give us the space ID.
 data "octopusdeploy_spaces" "spaces" {
   partial_name = var.octopus_space_name
-  skip         = 5
-  take         = 100
+  skip         = 0
+  take         = 1
 }
