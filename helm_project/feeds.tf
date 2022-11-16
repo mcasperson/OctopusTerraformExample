3400886639dbf89b6bfe9690b7aecc08736abef1
Matthew Casperson <matthewcasperson@gmail.com>
2022-11-16 12:47:29 +1000

    Fixed a typo

--- helm_project/feeds.tf
+++ helm_project/feeds.tf
@@ -1,10 +1,10 @@
 # https://registry.terraform.io/providers/OctopusDeployLabs/octopusdeploy/latest/docs/data-sources/feeds
 # Get the ID of the feed called "Docker Hub".
 data "octopusdeploy_feeds" "dockerhub" {
-  feed_type = "Docker"
-  name      = "Docker Hub"
-  skip      = 0
-  take      = 1
+  feed_type    = "Docker"
+  partial_name = "Docker Hub"
+  skip         = 0
+  take         = 1
 }
 
 # Get the ID of the built-in feed for the current space.
@@ -16,8 +16,8 @@ data "octopusdeploy_feeds" "built_in" {
 
 # Get the ID of the feed hosting the helm chart
 data "octopusdeploy_feeds" "helm_feed" {
-  feed_type = "Helm"
-  name      = var.helm_feed_name
-  skip      = 0
-  take      = 1
+  feed_type    = "Helm"
+  partial_name = var.helm_feed_name
+  skip         = 0
+  take         = 1
 }
